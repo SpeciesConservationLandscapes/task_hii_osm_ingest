d1c87bcc36d7f991f05a4a1e870dc349e6e28b49
Dustin Sampson <gridcell@gmail.com>
2021-03-30 11:02:06 -0700

    add formatter so defaults show up in help

--- src/task.py
+++ src/task.py
@@ -206,7 +206,7 @@ class HIIOSMIngest(HIITask):
                 self.clean_assets(_assets_to_clean)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("-d", "--taskdate", default=datetime.now(timezone.utc).date())
 
     parser.add_argument(
